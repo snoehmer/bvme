084d667fae781f23d6c59c663454ff9c2f4c5197
Stefan Noehmer <s.noehmer@student.tugraz.at>
2012-10-29 18:03:02 +0100

    fixed canny edge detector

--- assignment1/implementation/task3.m
+++ assignment1/implementation/task3.m
@@ -1,18 +1,18 @@
 % Assignment 1, Task 3: Canny Edge Detector
 
-clear all;
+%clear all;
 close all;
 
 
 %% common definitions
 
-%image_filename = '../images/butterfly.jpg';
-image_filename = 'testimage.png';
+image_filename = '../images/butterfly.jpg';
+%image_filename = 'testimage.png';
 
-gauss_sigma = 0.5;
+gauss_sigma = 0.7;
 
-threshold_low = 0.3;
-threshold_high = 0.7;
+threshold_low = 0.1;
+threshold_high = 0.5;
 
 
 %% read image
@@ -38,22 +38,19 @@ dimage_y = imfilter(image, kernel_sobel_y, 'replicate', 'conv');
 
 % calculate magnitude and angle of gradient
 image_magnitude = sqrt(dimage_x .^ 2 + dimage_y .^ 2);
-image_angle = atan2(dimage_y, dimage_x);
+image_angle = atan(dimage_x ./ dimage_y);
 
 
 %% step 3: perform non-maximum suppression
 
 % find principal direction of edges
-angle_limits = [-inf -7*pi/8:pi/4:7*pi/8 inf];
+angle_limits = [-inf -3*pi/8:pi/4:3*pi/8 inf];
 
 [~, angle_directions_full] = histc(image_angle, angle_limits);
 
-% combine the same directions: 1=0°, 2=45°, 3=90°, 4=135°
+% combine the same directions: 1=5=90°, 2=-45°, 3=0°, 4=45°
 angle_directions = angle_directions_full;
-angle_directions(angle_directions_full == 5 | angle_directions_full == 9) = 1;
-angle_directions(angle_directions_full == 6) = 2;
-angle_directions(angle_directions_full == 7) = 3;
-angle_directions(angle_directions_full == 8) = 4;
+angle_directions(angle_directions_full == 5) = 1;
 
 % generate image with replicated border
 image_magnitude_b = task2_border(image_magnitude, 1);
@@ -87,8 +84,8 @@ image_magnitude_diff2(angle_directions == 4) = image_magnitude(angle_directions 
 % suppress all pixels which are not the maximum
 image_nonmax = image_magnitude;
 
-image_nonmax(image_magnitude_diff1 <= 0) = 0;
-image_nonmax(image_magnitude_diff2 <= 0) = 0;
+image_nonmax(image_magnitude_diff1 < 0) = 0;
+image_nonmax(image_magnitude_diff2 < 0) = 0;
 
 
 %% step 4: double thresholding
@@ -145,6 +142,7 @@ while 1
     % else continue with the next iteration
     last_strong_edges = strong_edges;
     
+    % eye candy and window manager stress test: show progress of weak edge promotion
     %figure('name', ['strong edges for iteration ' num2str(iteration)]);
     %imshow(strong_edges + image_threshold);
     
@@ -153,6 +151,9 @@ end
 
 
 disp(['binary edge detector image complete after ' num2str(iteration) ' iterations']);
+
+
+%% show result
 
 % display binary strong edges matrix
 figure('name', 'Canny Edge Detector result');
