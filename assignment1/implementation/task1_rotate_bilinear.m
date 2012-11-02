83aaf0591de83bd80f511c24e8794e8b4d5fbac4
Stefan Noehmer <s.noehmer@student.tugraz.at>
2012-11-02 15:32:52 +0100

    removed unneccessary border handling from ass1 task1

--- assignment1/implementation/task1_rotate_bilinear.m
+++ assignment1/implementation/task1_rotate_bilinear.m
@@ -6,29 +6,19 @@ function [image_out] = task1_rotate_bilinear(image_in, angle)
     
     size_y = size(image_in, 1);
     size_x = size(image_in, 2);
-
-    % prepare input image for correct border handling
-    size_y_b = size(image_in, 1) + 2;
-    size_x_b = size(image_in, 2) + 2;
-    
-    image_in_b = zeros(size_y_b, size_x_b);
-    image_in_b(2:size_y_b-1, 2:size_x_b-1) = image_in(:,:);
     
     % calculate coordinates of image center
     y_center = ceil(size_y / 2);
     x_center = ceil(size_x / 2);
     
-    y_center_b = ceil(size_y_b / 2);
-    x_center_b = ceil(size_x_b / 2);
-    
     % calc minimum and maximum coordinates (with (0,0) at image center)
-    y_min_b = 1 - y_center_b;
-    y_max_b = y_min_b + size_y_b - 1;
-    x_min_b = 1 - x_center_b;
-    x_max_b = x_min_b + size_x_b - 1;
+    y_min = 1 - y_center;
+    y_max = y_min + size_y - 1;
+    x_min = 1 - x_center;
+    x_max = x_min + size_x - 1;
     
     % calc coordinates matrices for whole image (with (0,0) at image center)
-    [x_coords, y_coords] = meshgrid(x_min_b:x_max_b, y_min_b:y_max_b);
+    [x_coords, y_coords] = meshgrid(x_min:x_max, y_min:y_max);
     
     for y_out = 1:size_y
         
@@ -49,7 +39,7 @@ function [image_out] = task1_rotate_bilinear(image_in, angle)
             g_x = task1_g(diff_x);
             
             % multiply weights with pixels and sum up to get the output value
-            weighted_input = image_in_b .* g_x .* g_y;
+            weighted_input = image_in .* g_x .* g_y;
             
             image_out(y_out, x_out) = sum(weighted_input(:));
             
@@ -58,4 +48,3 @@ function [image_out] = task1_rotate_bilinear(image_in, angle)
     end
     
 end
-
